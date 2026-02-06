fcf05c24b555b076f5dfd437e9a3ae31add3b022
Kadurin Danylo <danil.kadurin.person@gmail.com>
2026-02-07 00:55:14 +0100

    fix-bug/api return array

--- src/js/sections/artists.js
+++ src/js/sections/artists.js
@@ -1,5 +1,9 @@
 import { getArtistsInfo } from '../api/artists-api';
 
-export const initArtists = async () => {
-  getArtistsInfo({ limit: 8 });
+export const initArtists = async ({ limit, page }) => {
+  return getArtistsInfo({ limit, page });
 };
+
+const page = 1;
+const params = { limit: 8, page };
+const responceArtists = await initArtists(params);
