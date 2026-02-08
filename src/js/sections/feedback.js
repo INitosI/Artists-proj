16275326420b93828e49dd0dd60c5d5550851950
Svitlana <svetlyashka2014@gmail.com>
2026-02-08 23:43:15 +0200

    Feedback swiper and pagination

--- src/js/sections/feedback.js
+++ src/js/sections/feedback.js
@@ -1,1 +1,33 @@
-export const initFeedback = () => {};
+import { Navigation, Pagination } from 'swiper/modules';
+
+/* ---------- Swiper ---------------------- */
+
+let swiperInstance = null;
+
+const initSwiper = slidesCount => {
+  if (swiperInstance) {
+    swiperInstance.destroy(true, true);
+  }
+  
+    swiperInstance = new Swiper('.feedback__slider', {
+    
+    modules: [Navigation, Pagination],
+    slidesPerView: 1,
+    spaceBetween: 24,
+    loop: false,
+
+    navigation: {
+      nextEl: '.feedback__nav--next',
+      prevEl: '.feedback__nav--prev',
+    },
+
+    on: {
+      init(swiper) {
+        updateFeedbackPagination(swiper.activeIndex, slidesCount);
+      },
+      slideChange(swiper) {
+        updateFeedbackPagination(swiper.activeIndex, slidesCount);
+      },
+    },
+  });
+};
