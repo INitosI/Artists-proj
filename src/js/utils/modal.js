d8f2e11d8853f4343044b4e5a5da43e6ebc138f6
Dima Karpinskyi <dimakjkarpinski@gmail.com>
2026-02-10 15:14:27 +0100

    modal-adaptive-style

--- src/js/utils/modal.js
+++ src/js/utils/modal.js
@@ -46,7 +46,8 @@ const renderModal = (artist, modalRoot) => {
   modalRoot.innerHTML = `
     <div class="modal__content" role="dialog" aria-modal="true">
 
-      <h2 class="modal__title">${artist.strArtist} <button class="modal__close-btn" aria-label="Close modal">&times;</button></h2>
+    <button class="modal__close-btn" aria-label="Close modal">&times;</button>
+      <h2 class="modal__title">${artist.strArtist}</h2>
 
       <div class="modal__body">
         <div class="modal__image-artist">
